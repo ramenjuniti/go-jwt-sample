02cf22d00cba6260304c98dbe6a7e50b58098375
ramenjuniti <ramenjuniti@gmail.com>
2020-01-09 17:52:25 +0900

    middlewareでトークンを確かめるぜーーーーー

--- server/main.go
+++ server/main.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/dgrijalva/jwt-go"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -39,9 +42,17 @@ func main() {
 
 	dbm.AddTableWithName(User{}, "user")
 
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("SECRET_KEY")), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
 	r := mux.NewRouter()
 	r.Methods(http.MethodPost).Path("/signup").HandlerFunc(signup)
 	r.Methods(http.MethodPost).Path("/login").HandlerFunc(login)
+	r.Methods(http.MethodGet).Path("/private").Handler(jwtMiddleware.Handler(http.HandlerFunc(private)))
 
 	log.SetFlags(log.Ldate + log.Ltime + log.Lshortfile)
 	log.SetOutput(os.Stdout)
